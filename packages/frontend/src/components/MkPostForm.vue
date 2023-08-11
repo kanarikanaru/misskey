f95a575856ceaf0690ba5708b34e42ae5d72ecc5
KanariKanaru <93921745+kanarikanaru@users.noreply.github.com>
2023-08-11 13:18:23 +0900

    CW時にwarningを表示する
    https://github.com/TeamNijimiss/misskey
    よりインポートし変更

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -64,6 +64,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 			<button class="_buttonPrimary" style="padding: 4px; border-radius: 8px;" @click="addVisibleUser"><i class="ti ti-plus ti-fw"></i></button>
 		</div>
 	</div>
+	<MkInfo v-if="useCw" warn :class="$style.contentWarningWarn">{{ i18n.ts.contentWarningWarn }}</MkInfo>
 	<MkInfo v-if="hasNotSpecifiedMentions" warn :class="$style.hasNotSpecifiedMentions">{{ i18n.ts.notSpecifiedMentionWarning }} - <button class="_textButton" @click="addMissingMention()">{{ i18n.ts.add }}</button></MkInfo>
 	<input v-show="useCw" ref="cwInputEl" v-model="cw" :class="$style.cw" :placeholder="i18n.ts.annotation" @keydown="onKeydown">
 	<div :class="[$style.textOuter, { [$style.withCw]: useCw }]">
@@ -1105,6 +1106,11 @@ defineExpose({
 	border-radius: 8px;
 	background: var(--X4);
 }
+
+.contentWarningWarn {
+	margin: 0 20px 16px 20px;
+}
+
 
 .hasNotSpecifiedMentions {
 	margin: 0 20px 16px 20px;
