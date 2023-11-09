ce74e0aa0520bd071851ef13c76970595e75cc7f
KanariKanaru <93921745+kanarikanaru@users.noreply.github.com>
2023-11-10 00:48:07 +0900

    lint

--- packages/backend/src/core/entities/UserEntityService.ts
+++ packages/backend/src/core/entities/UserEntityService.ts
@@ -337,7 +337,6 @@ export class UserEntityService implements OnModuleInit {
 		const isAdmin = isMe && opts.detail ? this.roleService.isAdministrator(user) : null;
 		const unreadAnnouncements = isMe && opts.detail ?
 			(await this.announcementService.getUnreadAnnouncements(user)).map((announcement) => ({
-				createdAt: this.idService.parse(announcement.id).date.toISOString(),
 				...announcement,
 			})) : null;
 
