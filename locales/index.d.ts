ebd3cdb40e561b67c4cbabeb75ec834cb77490b3
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-02-16 18:50:39 +0900

    enhance: 通知の履歴をリセットできるように

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -8867,6 +8867,10 @@ export interface Locale extends ILocale {
          * {n}人にフォローされました
          */
         "followedBySomeUsers": ParameterizedString<"n">;
+        /**
+         * 通知の履歴をリセットする
+         */
+        "flushNotification": string;
         "_types": {
             /**
              * すべて
