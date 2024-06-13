d8f74b4f51fdb502d333bb1969315ad6d8f85cd6
KanariKanaru <93921745+kanarikanaru@users.noreply.github.com>
2024-06-14 04:00:31 +0900

    Revert "lint fixes"
    This reverts commit d55e638a231b380e733dc0ada3c3de410f918a93.

--- packages/backend/src/postgres.ts
+++ packages/backend/src/postgres.ts
@@ -85,7 +85,7 @@ import { bindThis } from '@/decorators.js';
 
 export const dbLogger = new MisskeyLogger('db');
 
-const sqlLogger = dbLogger.createSubLogger('sql', 'gray');
+const sqlLogger = dbLogger.createSubLogger('sql', 'gray', false);
 
 class MyCustomLogger implements Logger {
 	@bindThis
